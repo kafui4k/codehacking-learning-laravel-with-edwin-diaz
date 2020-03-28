e1e4a7ffb8afbfb2387641c29772bd192de310f5
kafui4k <kalordo7@gmail.com>
2020-03-28 18:30:14 +0000

    admin master page setup

--- gulpfile.js
+++ gulpfile.js
@@ -12,5 +12,28 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss')
+
+        .style([
+
+            'libs/blog-post.css',
+            'libs/bootstrap.css',
+            'libs/font-awesome.css',
+            'libs/metisMenu.css',
+            'libs/sb-admin-2.css'
+
+        ], './public/css/libs.css')
+
+
+
+        .scripts([
+
+            'libs/jquery.js',
+            'libs/bootstrap.js',
+            'libs/sb-admin-2.js',
+            'libs/scripts.js'
+
+        ], './public/js/libs.js')
+
+
 });
